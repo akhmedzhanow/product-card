14ce6fd53572ed21535fdab814a8df046e87180b
akhmedzhanow <akhmedzhanow@gmail.com>
2025-12-27 20:17:00 +0400

    homework-11

---
+++ src/app/Cafe.js
@@ -0,0 +1,20 @@
+export class Cafe {
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
+  }
+
+  getInfo() {
+    return {
+      name: this.name,
+      location: this.location,
+    };
+  }
+
+  orderDrink(drink) {
+    console.log(`Заказ в "${this.name}" (${this.location}): ${drink.name}`);
+    drink.serve();
+    console.log(`Заказ завершён: ${drink.name}`);
+    console.log("Инфо напитка:", drink.getInfo());
+  }
+}
